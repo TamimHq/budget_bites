3d84cef7a7a6c0f02200bfc2d43cf5fa7ada402e
Tamim <86544999+TamimHq@users.noreply.github.com>
2023-10-22 22:51:03 +0600

    Update splash.dart

--- lib/boot_up/splash.dart
+++ lib/boot_up/splash.dart
@@ -3,54 +3,73 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class Splash extends StatefulWidget {
-  const Splash({super.key});
+  const Splash({Key? key}) : super(key: key);
 
   @override
   State<Splash> createState() => _SplashState();
 }
 
-class _SplashState extends State<Splash> with SingleTickerProviderStateMixin{
+class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _opacityAnimation;
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-    
-    Future.delayed(const Duration(seconds: 2),() {
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>const SignIn()),);
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    _opacityAnimation = Tween<double>(begin: 0, end: 1).animate(_controller);
+
+    _controller.forward();
+
+    Future.delayed(const Duration(seconds: 4), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const SignIn()),
+      );
     });
   }
 
   @override
-  void dispose(){
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,overlays: SystemUiOverlay.values);
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: SystemUiOverlay.values);
+    _controller.dispose();
     super.dispose();
-
   }
-  @override
-  Widget build(BuildContext context) => Builder(
-    builder: (context) {
-      return Scaffold(
-          body:Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.red.shade800,Colors.purpleAccent],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              )
-            ),
 
-              child:  Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/images/LOGO.png'),
-                  const Text('Budget Bites',style: TextStyle(fontSize: 30,fontWeight: FontWeight.w600,fontFamily: 'Poppins'),)
-                ],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.red.shade800, Colors.purpleAccent],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FadeTransition(
+                opacity: _opacityAnimation,
+                child: Image.asset('assets/images/LOGO.png'),
               ),
-            ),
-          );
-    }
-  );
+              const Text(
+                'Budget Bites',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600, fontFamily: 'Poppins'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
