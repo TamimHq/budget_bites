35922ea5e18097785a2262c076b102881c7eb61c
Tamim <86544999+TamimHq@users.noreply.github.com>
2023-12-28 22:37:14 +0600

    delete

--- budget_bites_store_interface/android/app/src/main/java/com/example/budgets_bites/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.budgets_bites;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- budget_bites_store_interface/android/app/src/main/kotlin/com/example/budget_bites_store_interface/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.budget_bites_store_interface
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
