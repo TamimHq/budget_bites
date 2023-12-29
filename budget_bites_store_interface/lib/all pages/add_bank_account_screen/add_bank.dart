708bf9d87101b72ee5cbcaf2aa051f12fb1f9b4a
Tamim <86544999+TamimHq@users.noreply.github.com>
2023-12-29 21:47:26 +0600

    new

---
+++ budget_bites_store_interface/lib/all pages/add_bank_account_screen/add_bank.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class AddBankScreen extends StatelessWidget{
+  const AddBankScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
