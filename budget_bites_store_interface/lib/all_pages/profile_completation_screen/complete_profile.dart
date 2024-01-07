0e3dd7fc5ecd694018aaa3392bc9201f6d1bb8f7
Tamim <86544999+TamimHq@users.noreply.github.com>
2024-01-07 12:52:21 +0600

    update

--- budget_bites_store_interface/lib/all_pages/profile_completation_screen/complete_profile.dart
+++ budget_bites_store_interface/lib/all_pages/profile_completation_screen/complete_profile.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import '../../widgets/custom_image_view.dart';
+
 class CompleteProfileScreen extends StatefulWidget{
   const CompleteProfileScreen({super.key});
 
@@ -17,9 +19,10 @@ class _CompleteProfileScreenState extends State<CompleteProfileScreen> {
     // TODO: implement build
     //throw UnimplementedError();
     return Scaffold(
+
         appBar: AppBar(
-          toolbarHeight: 60,
-          backgroundColor: Colors.pink,
+
+          backgroundColor: Color(0xFFEA2242),
           leading: IconButton(onPressed: () {},
               icon: const Icon(Icons.arrow_back_ios_new, color: Colors.white,)),
 
@@ -30,135 +33,213 @@ class _CompleteProfileScreenState extends State<CompleteProfileScreen> {
             ),
 
           ),
-
+          automaticallyImplyLeading: false,
         ),
 
-        body: Center(
-          child: Container(
-            width: 350,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
 
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: const BorderSide(
-                            color: Colors.yellowAccent,
-                          )
+        body: Form(
+            child: Container(
+              child: SizedBox(
+                width: MediaQuery.sizeOf(context).width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 80,),
 
+                    TopDesign(),
+
+                    SizedBox(height: 28,),
+                    TextFormField(
+
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: const BorderSide(
+                                color: Colors.yellowAccent,
+                              )
+
+                          ),
+
+                          hintText: "Your Username"
                       ),
 
-                      hintText: "Your Username"
-                  ),
 
+                    ),
+                    const SizedBox(height: 5,),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: const BorderSide(
+                                color: Colors.yellowAccent,
+                              )
 
-                ),
-                const SizedBox(height: 5,),
-                TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: const BorderSide(
-                            color: Colors.yellowAccent,
-                          )
+                          ),
 
+                          hintText: "FullName"
                       ),
 
-                      hintText: "FullName"
-                  ),
+                    ),
+                    const SizedBox(height: 5,),
 
-                ),
-                const SizedBox(height: 5,),
-
-                TextField(
-                  controller: date,
-                  decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.calendar_today),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      labelText: "Select your Birth Date"
-                  ),
-
-                  onTap: () async {
-                    DateTime? pickedDate = await showDatePicker(
-                      context: context,
-
-                      firstDate: DateTime(1950),
-                      lastDate: DateTime(2015),
-                    );
-                    if (pickedDate != null) {
-                      setState(() {
-                        date.text = DateFormat.yMMMMEEEEd().format(pickedDate);
-                      });
-                    }
-                  },
-
-                ),
-                const SizedBox(height: 5,),
-                TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: const BorderSide(
-                            color: Colors.yellowAccent,
-                          )
-
+                    TextFormField(
+                      controller: date,
+                      decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.calendar_today),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          labelText: "Select your Birth Date"
                       ),
 
-                      hintText: "Name of the Store"
-                  ),
+                      onTap: () async {
+                        DateTime? pickedDate = await showDatePicker(
+                          context: context,
 
-                ),
-                const SizedBox(height: 5,),
-                TextField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: const BorderSide(
-                            color: Colors.yellowAccent,
-                          )
+                          firstDate: DateTime(1950),
+                          lastDate: DateTime(2015),
+                        );
+                        if (pickedDate != null) {
+                          setState(() {
+                            date.text = DateFormat.yMMMMEEEEd().format(pickedDate);
+                          });
+                        }
+                      },
 
+                    ),
+                    const SizedBox(height: 5,),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: const BorderSide(
+                                color: Colors.yellowAccent,
+                              )
+
+                          ),
+
+                          hintText: "Name of the Store"
                       ),
 
-                      hintText: "Store Location"
-                  ),
+                    ),
+                    const SizedBox(height: 5,),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: const BorderSide(
+                                color: Colors.yellowAccent,
+                              )
 
+                          ),
+
+                          hintText: "Store Location"
+                      ),
+
+                    ),
+                    const SizedBox(height: 10,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Complete', style: TextStyle(fontSize: 20,),
+                          ),
+                          style: ButtonStyle(
+                            backgroundColor:
+                            MaterialStateProperty.all(Colors.red),
+                            foregroundColor:
+                            MaterialStateProperty.all(Colors.white),
+                            padding: MaterialStateProperty.all(
+                                EdgeInsets.symmetric(
+                                    horizontal:
+                                    MediaQuery
+                                        .of(context)
+                                        .size
+                                        .width *
+                                        0.1)),
+                            shape: MaterialStateProperty.all(
+                                const RoundedRectangleBorder(
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(50)),
+                                )),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 10,),
-                ElevatedButton(onPressed: () {},
-                  child: const Text(
-                    'Complete', style: TextStyle(fontSize: 20,),
-                  ),
-                  style: ButtonStyle(
-                    backgroundColor:
-                    MaterialStateProperty.all(Colors.red),
-                    foregroundColor:
-                    MaterialStateProperty.all(Colors.white),
-                    padding: MaterialStateProperty.all(
-                        EdgeInsets.symmetric(
-                            horizontal:
-                            MediaQuery
-                                .of(context)
-                                .size
-                                .width *
-                                0.1)),
-                    shape: MaterialStateProperty.all(
-                        const RoundedRectangleBorder(
-                          borderRadius:
-                          BorderRadius.all(Radius.circular(50)),
-                        )),
-                  ),
-                ),
-              ],
+              ),
+
+
             ),
 
 
-          ),
+        ),
+       extendBodyBehindAppBar: true,
+    );
+  }
+}
 
-        )
+class TopDesign extends StatelessWidget {
+  const TopDesign({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+    height: 163,
+    width: MediaQuery.sizeOf(context).width,
+    child: Stack(
+        alignment: Alignment.topLeft,
+        children: [
+      CustomImageView(
+          imagePath: 'asset/images/img_ellipse.png',
+          height: 363,
+          width: MediaQuery.sizeOf(context).width,
+          alignment: Alignment.center),
+      Align(
+          alignment: Alignment.topLeft,
+          child: Padding(
+              padding: EdgeInsets.only(left: 33, top: 60),
+              child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+
+                          SizedBox(height: 30),
+                         Padding(
+                          padding: EdgeInsets.only(right: 57),
+
+                            child: InkWell(
+                              onTap: (){},
+                              child: Container(
+                                 height: 61,
+                                  width: 60,
+                                padding: EdgeInsets.all(8),
+                                decoration: BoxDecoration(
+                                color: Colors.yellow,
+                                ),
+                               alignment: Alignment.centerRight,
+                                  child: CustomImageView(
+                                  imagePath: 'asset/images/img_camera.svg')
+                                                  ),
+                            ),
+                ),
+
+              ]
+                )
+              ]
+              )
+          ),
+      ),
+    ]
+    )
     );
   }
 }
